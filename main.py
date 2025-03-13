fb0807d910a79de293b9ba86c9f3404d59452524
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-13 14:31:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,50 @@
 
 import turtle
 import pandas
-from states import State
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-print(screen.screensize())
 
 data = pandas.read_csv("50_states.csv")
-df = pandas.DataFrame(data)
+all_states = data.state.to_list()
 
-states_correct = 0
 
-states_list = []
+guessed_states = []
 
-# create a while loop to continue playing the game
+while len(guessed_states) < 50:
+    # collect user input and save to a variable
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
 
-game_is_on = True
+    # create conditional for when the user wants to exit the game
+    if answer_state == "Exit":
+        # create a new list for states that have not been guessed yet
+        missing_states = [state for state in all_states if state not in guessed_states]
 
-while game_is_on:
+        # create a new data frame from the list
+        new_data = pandas.DataFrame(missing_states)
 
-    # collect the user input and save to a variable
-    answer_state = screen.textinput(title=f"{states_correct}/50 States Correct", prompt="What's another state's name?").lower().capitalize()
-
-    # create a variable storing the row of data from the data frame based that matches the user input
-    answer_state_row = df[df.state == answer_state]
-    print(answer_state_row)
-
-    # check if the user input is contained in the data frame
-    # mask will return a boolean for each value in the data frame based on if it matches or not
-    # matches = True , does not match = False
-    mask = df.isin([answer_state])
-    print(mask)
-    # checks the mask to see whether there are any pieces of data met the criteria and if the state has already been guessed
-    # if yes, run the program. if no, inform the user that the input was not valid.
-    if mask.any().any() and answer_state not in states_list:
-        # remove the "dtype" and extra information that was accompanying the state name
-        answer_state_var = str(answer_state_row.state.values)
-        # remove the brackets and apostrophes that were accompanying the value
-        answer_state_final =answer_state_var.strip("['']")
-
-        # save the x and y coordinates from the guessed state into their own values
-        answer_state_x = int(answer_state_row.x)
-        answer_state_y = int(answer_state_row.y)
-
-        # create a state and place it in the correct position
-        state = State(answer_state_final,answer_state_x,answer_state_y)
-
-        # add correctly guessed state to states list
-        states_list.append(answer_state)
-
-        # add a score to the title screen
-        states_correct += 1
+        # save the new data frame to a csv
+        new_data.to_csv("states_to_learn.csv")
+        break
+        
+    # create conditional to check if answer was already guessed or if it was not a valid entry  
+    if answer_state in guessed_states:
+        print("You already guessed that state")
+   
+    elif answer_state in all_states:
+        # append the input to the guessed states list
+        guessed_states.append(answer_state)
+        
+        # if they get it right, create a turtle at the correct x and y coordinates
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.item()), int(state_data.y.item()))
+        t.write(state_data.state.item())
     else:
-        print(f"Try Again, {answer_state} is not a US state.")
-
-screen.exitonclick()
+        print(f"Try again, {answer_state} is not a US state.")
